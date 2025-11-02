6c275707059005a323db6e0af1d21a0b796b9c67
angel <a20220729@pucp.edu.pe>
2025-11-02 16:26:38 -0500

    cambios en la interfaz de registro

--- Kat1GUI/RegistroForm.h
+++ Kat1GUI/RegistroForm.h
@@ -40,7 +40,7 @@ namespace Kat1GUI {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
+
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Label^ label2;
@@ -50,6 +50,7 @@ namespace Kat1GUI {
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ labelTituloPlantas;
 	protected:
 
 	private:
@@ -65,7 +66,6 @@ namespace Kat1GUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label2 = (gcnew System::Windows::Forms::Label());
@@ -75,17 +75,8 @@ namespace Kat1GUI {
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->labelTituloPlantas = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(87, 35);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(82, 13);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"¡REGÍSTRATE!";
-			this->label1->Click += gcnew System::EventHandler(this, &RegistroForm::label1_Click);
 			// 
 			// button1
 			// 
@@ -110,7 +101,7 @@ namespace Kat1GUI {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(23, 72);
+			this->label2->Location = System::Drawing::Point(24, 85);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(96, 13);
 			this->label2->TabIndex = 3;
@@ -119,7 +110,7 @@ namespace Kat1GUI {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(23, 97);
+			this->label3->Location = System::Drawing::Point(24, 110);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(61, 13);
 			this->label3->TabIndex = 4;
@@ -127,32 +118,32 @@ namespace Kat1GUI {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(125, 72);
+			this->textBox1->Location = System::Drawing::Point(126, 85);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->Size = System::Drawing::Size(146, 20);
 			this->textBox1->TabIndex = 5;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(125, 94);
+			this->textBox2->Location = System::Drawing::Point(126, 110);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->Size = System::Drawing::Size(146, 20);
 			this->textBox2->TabIndex = 6;
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(90, 179);
+			this->button3->Location = System::Drawing::Point(100, 179);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(75, 23);
 			this->button3->TabIndex = 7;
-			this->button3->Text = L"registrar";
+			this->button3->Text = L"Registrate";
 			this->button3->UseVisualStyleBackColor = true;
 			this->button3->Click += gcnew System::EventHandler(this, &RegistroForm::button3_Click);
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(23, 123);
+			this->label4->Location = System::Drawing::Point(24, 136);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(32, 13);
 			this->label4->TabIndex = 8;
@@ -160,16 +151,29 @@ namespace Kat1GUI {
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(125, 120);
+			this->textBox3->Location = System::Drawing::Point(126, 133);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(147, 20);
 			this->textBox3->TabIndex = 9;
+			// 
+			// labelTituloPlantas
+			// 
+			this->labelTituloPlantas->AutoSize = true;
+			this->labelTituloPlantas->Font = (gcnew System::Drawing::Font(L"Calibri", 14, System::Drawing::FontStyle::Bold));
+			this->labelTituloPlantas->Location = System::Drawing::Point(96, 33);
+			this->labelTituloPlantas->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->labelTituloPlantas->Name = L"labelTituloPlantas";
+			this->labelTituloPlantas->Size = System::Drawing::Size(89, 23);
+			this->labelTituloPlantas->TabIndex = 23;
+			this->labelTituloPlantas->Text = L"REGISTRO";
+			this->labelTituloPlantas->Click += gcnew System::EventHandler(this, &RegistroForm::labelTituloPlantas_Click);
 			// 
 			// RegistroForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->labelTituloPlantas);
 			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->button3);
@@ -179,7 +183,6 @@ namespace Kat1GUI {
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->label1);
 			this->Name = L"RegistroForm";
 			this->Text = L"RegistroForm";
 			this->Load += gcnew System::EventHandler(this, &RegistroForm::RegistroForm_Load);
@@ -232,5 +235,7 @@ namespace Kat1GUI {
 		
 	}
 	
+private: System::Void labelTituloPlantas_Click(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
